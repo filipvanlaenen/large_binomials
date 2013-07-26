90efc517fc485440c7ee7041ae808f5518fecd3e
Filip van Laenen <f.a.vanlaenen@ieee.org>
2013-07-26 20:43:28 +0200

    Updated the license to GPLv3

--- lib/large_binomials.rb
+++ lib/large_binomials.rb
@@ -1,3 +1,23 @@
+# encoding: utf-8
+#
+# Ruby Library to Calculate Large Binomials
+# Copyright © 2013 Filip van Laenen <f.a.vanlaenen@ieee.org>
+#
+# This file is part of the Ruby Library to Calculate Large Binomials.
+#
+# The Ruby Library to Calculate Large Binomials is free software: you can
+# redistribute it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+# 
+# The Ruby Library to Calculate Large Binomials is distributed in the hope that
+# it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+# Public License for more details.
+# 
+# You can find a copy of the GNU General Public License in /LICENSE
+#
+
 # Library namespace
 module LargeBinomials
 end
